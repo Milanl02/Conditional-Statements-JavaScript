ec7a6413428da90ee383734bdbf7cc0dc91be2c6
Milanl02 <89406051+Milanl02@users.noreply.github.com>
2021-09-17 23:01:28 -0400

    Second Commit
    I had to go back and add some more code to complete the equation entered by the user.

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ var inputedOperator=document.body.querySelector(".operator");
 var secondInput=document.body.querySelector(".secondNumber");
 var equation=document.body.querySelector(".answer")
 
-var q1=prompt("Enter your first number:");
+var q1=Number(prompt("Enter your first number:"));
 
 if(isNaN(q1)){
-    firstInput.innerHTML="First step done incorrectly. You did not enter a number in the first prompt. Program has ended.";
+    firstInput.innerHTML="First step done incorrectly. You did not enter a number in the first prompt. Program has failed here.";
 }else{
     firstInput.innerHTML="First step done correctly. Your number was saved by the calculator.";
 }
@@ -16,31 +16,25 @@ var q2=prompt("Enter an operator: +, -, *, x, /:")
 if(q2=="+"||q2=="-"||q2=="*"||q2=="x"||q2=="/"){
     inputedOperator.innerHTML="Second step done correctly. Your operator was saved by the calculator.";
 }else{
-    inputedOperator.innerHTML="Second step done incorrectly. You did not enter an operator in the second prompt. Program has ended.";
+    inputedOperator.innerHTML="Second step done incorrectly. You did not enter an operator in the second prompt. Program has failed here.";
 }
 
-var q3=prompt("Enter your second number:");
+var q3=Number(prompt("Enter your second number:"));
 
 if(isNaN(q3)){
-    secondInput.innerHTML="Third step done incorrectly. You did not enter a number here. Program has ended. ";
+    secondInput.innerHTML="Third step done incorrectly. You did not enter a number here. Program has failed here. ";
 }else{
     secondInput.innerHTML="Third step done correctly. Your number was saved by the calculator.";
 }
 
-var q1=number(q1)
-var q3=number(q3)
-
-var later=(q1)(q2)(q3);
-    equation.innerHTML="The answer to your equation is (later)";
-
-
-
-
-
-
-
-
-
-
-    // if(userOperator=="+")||(userOperator=="-")
-
+if(q2=="+"){
+   equation.innerHTML=q1+q3
+}else if(q2=="-"){
+    equation.innerHTML=q1-q3
+}else if(q2=="*"){
+    equation.innerHTML=q1*q3
+}else if(q2=="x") {
+    equation.innerHTML = q1*q3
+}else if(q2=="/"){
+    equation.innerHTML=q1/q3
+}
